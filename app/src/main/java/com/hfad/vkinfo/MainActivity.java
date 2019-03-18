e60e0debd39f631ffb60b9dea27fabf491d749b3
shumvlesu <sashumihin@gmail.com>
2019-03-18 14:04:29 +0300

    5 урок

--- app/src/main/java/com/hfad/vkinfo/MainActivity.java
+++ app/src/main/java/com/hfad/vkinfo/MainActivity.java
@@ -2,12 +2,33 @@ package com.hfad.vkinfo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText searchField = findViewById(R.id.et_search_field);
+        Button searchButton =  findViewById(R.id.b_search_vk);
+
+        result = findViewById(R.id.tv_result);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result.setText("Кнопка была нажата.");
+            }
+        };
+
+        searchButton.setOnClickListener(onClickListener);
+
     }
 }
